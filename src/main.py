083c4c8eacf7a1fe62c9643c956e5e6cd5c97323
Lars-Water <larryvanderwater@hotmail.com>
2023-08-08 16:28:10 +0200

    changed base path

--- src/main.py
+++ src/main.py
@@ -46,5 +46,5 @@ def main(base_path):
 
 
 if __name__ == "__main__":
-    base_path = '/mnt/c/users/larry/Desktop/SE_Thesis/HH-Implementation/hh-dse-2.0'
+    base_path = '/mnt/c/users/larry/Desktop/SE_Thesis/hh-dse-2.0/Search_Method'
     main(base_path)
