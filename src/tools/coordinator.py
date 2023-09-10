03f762c837238f7003c5522f39062c87b2da3e1f
Art Vandelay <54835307+Lars-Water@users.noreply.github.com>
2023-09-10 16:08:52 +0200

    added coordinator

---
+++ src/tools/coordinator.py
@@ -0,0 +1,33 @@
+import os
+import json
+import subprocess
+
+class HeuristicSimulationCoordinator:
+    
+    def __init__(self):
+        # Initialize variables or configurations
+        pass
+    
+    def configure_model_boundaries(self, config):
+        # Code to configure model boundaries
+        pass
+    
+    def configure_and_run_simulation(self, config):
+        # Code to configure and run simulation
+        pass
+    
+    def process_simulation_output(self, output):
+        # Code to process simulation output and provide feedback to hyper-heuristic
+        pass
+    
+    def call_simulation_run(self, config):
+        # Code to initiate a simulation run
+        pass
+    
+    def return_simulation_output(self):
+        # Code to return the performance output of the simulation run
+        pass
+
+if __name__ == "__main__":
+    coordinator = HeuristicSimulationCoordinator()
+    # Test the methods
