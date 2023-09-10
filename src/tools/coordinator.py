510b07ecae00985396fd657f6816a24c2f7403a5
Art Vandelay <54835307+Lars-Water@users.noreply.github.com>
2023-09-10 17:44:59 +0200

    current state of coordinator

--- src/tools/coordinator.py
+++ src/tools/coordinator.py
@@ -1,33 +1,45 @@
-import os
-import json
+# Assuming you can import these modules
+from src.external import HERMAN_MODEL, CUSTOMHys
 import subprocess
 
 class HeuristicSimulationCoordinator:
     
     def __init__(self):
-        # Initialize variables or configurations
-        pass
-    
-    def configure_model_boundaries(self, config):
-        # Code to configure model boundaries
-        pass
-    
-    def configure_and_run_simulation(self, config):
-        # Code to configure and run simulation
-        pass
-    
-    def process_simulation_output(self, output):
-        # Code to process simulation output and provide feedback to hyper-heuristic
+        # TODO - Initialize the OMNeT++ and CUSTOMHys frameworks
         pass
     
     def call_simulation_run(self, config):
-        # Code to initiate a simulation run
-        pass
+        try:
+            # Configure the simulation
+            pass
+            
+            # Run the simulation and capture the output
+            pass
+            
+        except Exception as e:
+            print(f"An error occurred while running the simulation: {e}")
+            return None
     
-    def return_simulation_output(self):
-        # Code to return the performance output of the simulation run
-        pass
+    def configure_model_boundaries(self, boundaries):
+        # Configure the CUSTOMHys framework
+        self.customhys.configure_boundaries(boundaries)
+    
+    def process_simulation_output(self, output):
+        # Process the simulation output and use it as feedback for the hyper-heuristic
+        feedback = self.omnet.process_output(output)
+        self.customhys.use_feedback(feedback)
 
+# Example usage
 if __name__ == "__main__":
     coordinator = HeuristicSimulationCoordinator()
-    # Test the methods
+    
+    # Configure model boundaries
+    boundaries = {"param1": "value1", "param2": "value2"}
+    coordinator.configure_model_boundaries(boundaries)
+    
+    # Run simulation
+    config = {"setting1": "value1", "setting2": "value2"}
+    simulation_output = coordinator.call_simulation_run(config)
+    
+    # Process output
+    coordinator.process_simulation_output(simulation_output)
