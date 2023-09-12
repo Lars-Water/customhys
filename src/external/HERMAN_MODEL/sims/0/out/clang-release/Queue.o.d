6148720c2294d8f2fde91e550ec93a04f6bd5aba
Lars-Water <larryvanderwater@hotmail.com>
2023-09-12 12:01:31 +0000

    check error logs

--- src/external/HERMAN_MODEL/sims/0/out/clang-release/Queue.o.d
+++ src/external/HERMAN_MODEL/sims/0/out/clang-release/Queue.o.d
@@ -1,4 +1,4 @@
 out/clang-release//Queue.o: Queue.cc \
-  /home/lars-water/omnetpp-6.0.1/include/omnetpp.h
+  /home/larryvanderwater/omnetpp-6.0.1/include/omnetpp.h
 
-/home/lars-water/omnetpp-6.0.1/include/omnetpp.h:
+/home/larryvanderwater/omnetpp-6.0.1/include/omnetpp.h:
