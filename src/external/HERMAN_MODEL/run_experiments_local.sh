a67acdf15cfc45c557f1c94b23b597b6e0e364de
Art Vandelay <lars.vanderwater@student.uva.nl>
2023-09-17 13:10:11 +0200

    Revert "try to run experiments instead in codespace"
    This reverts commit 03ae8c664830da40a0e8cf9c678ab7478ef38c95.

--- src/external/HERMAN_MODEL/run_experiments_local.sh
+++ src/external/HERMAN_MODEL/run_experiments_local.sh
@@ -6,24 +6,24 @@
 # source setenv
 # cd $CUR
 # python3 experiments.py --platform local --sims_path "/mnt/c/Users/larry/Desktop/SE_thesis/hh-dse-2.0/src/external/Scalable-Simulation-DCPS-main/sims" --dummy_path "/mnt/c/Users/larry/Desktop/SE_thesis/hh-dse-2.0/src/external/Scalable-Simulation-DCPS-main/sims" --inet_path "/home/lars-water/omnetpp-6.0.1/omnet-projects/inet4.5" --experiments_path /mnt/c/Users/larry/Desktop/SE_thesis/hh-dse-2.0/src/external/Scalable-Simulation-DCPS-main/experiments
-# GITHUB CODESPACES
-CUR=$(pwd)
-cd /workspaces/hyper-heuristic-dse-2.0/omnetpp-6.0.1
-source setenv
-cd /workspaces/hyper-heuristic-dse-2.0/omnetpp-6.0.1/inet4.5
-source setenv
-cd $CUR
-python3 experiments.py --platform local --sims_path "/workspaces/hyper-heuristic-dse-2.0/src/external/HERMAN_MODEL/sims" --dummy_path "/workspaces/hyper-heuristic-dse-2.0/src/external/HERMAN_MODEL/sims" --inet_path "/workspaces/hyper-heuristic-dse-2.0/omnetpp-6.0.1/inet4.5" --experiments_path /workspaces/hyper-heuristic-dse-2.0/src/external/HERMAN_MODEL/experiments
-# GITHUB CODESPACES experiment_seq
+# # GITHUB CODESPACES
 # CUR=$(pwd)
 # cd /workspaces/hyper-heuristic-dse-2.0/omnetpp-6.0.1
 # source setenv
 # cd /workspaces/hyper-heuristic-dse-2.0/omnetpp-6.0.1/inet4.5
 # source setenv
 # cd $CUR
-# python experiment_seq.py \
-#   --data_path=/workspaces/hyper-heuristic-dse-2.0/src/external/HERMAN_MODEL/experiments/ \
-#   --inet_path=/workspaces/omnetpp-6.0.1/inet4.5/ \
-#   --sims_path=/workspaces/hyper-heuristic-dse-2.0/src/external/HERMAN_MODEL/sims/ \
-#   --dummy_path=/workspaces/hyper-heuristic-dse-2.0/src/external/HERMAN_MODEL/sims/ \
-#   --platform=local
+# python3 experiments.py --platform local --sims_path "/workspaces/hyper-heuristic-dse-2.0/src/external/HERMAN_MODEL/sims" --dummy_path "/workspaces/hyper-heuristic-dse-2.0/src/external/HERMAN_MODEL/sims" --inet_path "/workspaces/hyper-heuristic-dse-2.0/omnetpp-6.0.1/inet4.5" --experiments_path /workspaces/hyper-heuristic-dse-2.0/src/external/HERMAN_MODEL/experiments
+# GITHUB CODESPACES experiment_seq
+CUR=$(pwd)
+cd /workspaces/hyper-heuristic-dse-2.0/omnetpp-6.0.1
+source setenv
+cd /workspaces/hyper-heuristic-dse-2.0/omnetpp-6.0.1/inet4.5
+source setenv
+cd $CUR
+python experiment_seq.py \
+  --data_path=/workspaces/hyper-heuristic-dse-2.0/src/external/HERMAN_MODEL/experiments/ \
+  --inet_path=/workspaces/omnetpp-6.0.1/inet4.5/ \
+  --sims_path=/workspaces/hyper-heuristic-dse-2.0/src/external/HERMAN_MODEL/sims/ \
+  --dummy_path=/workspaces/hyper-heuristic-dse-2.0/src/external/HERMAN_MODEL/sims/ \
+  --platform=local
