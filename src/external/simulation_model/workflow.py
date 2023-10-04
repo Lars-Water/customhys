d640d0507b34f5ef316d6acad1061431aa389202
Art Vandelay <54835307+Lars-Water@users.noreply.github.com>
2023-10-04 21:01:54 +0000

    vec files are not moved?

--- src/external/simulation_model/workflow.py
+++ src/external/simulation_model/workflow.py
@@ -50,8 +50,8 @@ def main():
 
     num_sims = 1
 
-    sim_instances = [create_sim_pdes_comm(config, dummy_sim_path, id, inet_base) for id in range(num_sims)]
-    # sim_instances = [create_tictoc(config, dummy_sim_path, id, inet_base) for id in range(num_sims)]
+    # sim_instances = [create_sim_pdes_comm(config, dummy_sim_path, id, inet_base) for id in range(num_sims)]
+    sim_instances = [create_tictoc(config, dummy_sim_path, id, inet_base) for id in range(num_sims)]
     # sim_instances = [create_sim_custom_dummy(config, dummy_sim_path, id, inet_base) for id in range(num_sims)]
     # sim_instances = [create_sim_seq_comm(config, dummy_sim_path, id, inet_base) for id in range(num_sims)]
 
