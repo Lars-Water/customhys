d6cba7367f9d5d17621c752cc403a583124b0ffb
Art Vandelay <lars.vanderwater@student.uva.nl>
2023-10-05 09:30:09 +0200

    skip last columns

--- src/visualization/visualization.py
+++ src/visualization/visualization.py
@@ -41,9 +41,17 @@ def save_simulation_fitness(hist_values, store_path):
 '''
     Read the csv file in path /workspaces/hyper-heuristic-dse-2.0/src/external/simulation_model/workflow/results/ab5f71b5e22b1928ce6aeae0ae2aaadb/tictoc.csv
     to a dataframe and print the dataframe with column 'type' having value 'statistic'.
+
+    skip on columns 'underflows,overflows,binedges,binvalues'
 '''
 def save_hop_count():
-    df = pd.read_csv("/workspaces/hyper-heuristic-dse-2.0/src/external/simulation_model/workflow/results/ab5f71b5e22b1928ce6aeae0ae2aaadb/tictoc.csv")
+    df = pd.read_csv(
+        "/workspaces/hyper-heuristic-dse-2.0/src/external/simulation_model/workflow/results/ab5f71b5e22b1928ce6aeae0ae2aaadb/tictoc.csv", 
+        skipinitialspace=True, 
+        skiprows=0, 
+        skip_blank_lines=True, 
+        usecols=lambda x: x not in ['underflows','overflows','binedges','binvalues']
+    )
     print(df[df['type'] == 'statistic'])
 
 if __name__ == "__main__":
