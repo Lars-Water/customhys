cdc6fd804e7586eacdf8049c44b2298b54e80050
Art Vandelay <lars.vanderwater@student.uva.nl>
2023-10-25 09:56:06 +0200

    check new param value configuration

--- src/main.py
+++ src/main.py
@@ -16,6 +16,8 @@ def main(base_path):
 
     heur_sim_coordinator = coordinator.HeuristicSimulationCoordinator("/workspaces/hyper-heuristic-dse-2.0/config/coordinator.json") # noqa 501
 
+    print( heur_sim_coordinator.set_param_values([1,2,3,4]) )
+    
     # # Collect the simulation stats from the simulation run.
     # uid = "491e65ec2f6a2342fe509480d09a5987"
     # # heur_sim_coordinator.transform_scalar_files(uid)
