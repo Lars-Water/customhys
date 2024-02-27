b8c85b0dd6e4ad9640f5fab0f9ea75f1730c8295
Lars-Water <larryvanderwater@hotmail.com>
2024-02-27 23:41:17 +0100

    added vizualization config file to ignore and set omnetpp lib dir to LD path centralized bash script

--- main_das.sh
+++ main_das.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-# Specify the path to the base directory of the hyper-heuristic DSE here.
-BASE_DIR="/home/lvdwater/hyper-heuristic-dse-2.0"
-# Specify the path to the heuristic run configuration file here
-CONFIG_PATH_HEUR_RUN="/home/lvdwater/hyper-heuristic-dse-2.0/config/config_heuristic_run.json"
-# Specify the path to the coordinator configuration file here
-CONFIG_PATH_COORDINATOR="/home/lvdwater/hyper-heuristic-dse-2.0/config/config_coordinator.json"
-
 # Activate upper bash command to set conda environment.
 eval "$(/home/lvdwater/miniconda3/bin/conda shell.bash hook)"
+
+# Specify the path to the base directory of the hyper-heuristic DSE here.
+BASE_PATH="/home/lvdwater/hyper-heuristic-dse-2.0"
+# Specify the path to the heuristic run configuration file here
+CONFIG_PATH_HEUR_RUN="${BASE_PATH}/config/config_heuristic_run.json"
+# Specify the path to the coordinator configuration file here
+CONFIG_PATH_COORDINATOR="${BASE_PATH}/config/config_coordinator.json"
+
+# Define PYTHONPATH to Herman's framework.
+export PYTHONPATH="${PYTHONPATH}:${BASE_PATH}/src_main/external/simulation_model"
+
+# Define OMNET++ shared library to link loader.
+OMNET_BASE="/var/scratch/lvdwater/omnetpp-6.0.1"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${OMNET_BASE}/lib/
+
 # conda config --set auto_activate_base false
-cd /var/scratch/lvdwater/omnetpp-6.0.1/
+cd ${OMNET_BASE}
 source setenv
 cd ../inet4.5
 source setenv
 
 cd ~/hyper-heuristic-dse-2.0
-python3 main.py --base_dir "$BASE_DIR" --heur_run_config "$CONFIG_PATH_HEUR_RUN" --coordinator_config "$CONFIG_PATH_COORDINATOR"
+python3 main.py --base_path "$BASE_PATH" --heur_run_config "$CONFIG_PATH_HEUR_RUN" --coordinator_config "$CONFIG_PATH_COORDINATOR"
