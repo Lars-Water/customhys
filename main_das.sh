bea5543fe5bcbf1241022d94efe72dd46e5425fa
Lars-Water <larryvanderwater@hotmail.com>
2024-03-01 10:39:58 +0100

    set up single agent run

--- main_das.sh
+++ main_das.sh
@@ -22,8 +22,10 @@ cd ../inet4.5
 source setenv
 
 cd ~/hyper-heuristic-dse-2.0
+CONFIG_PATH_HEUR_RUN="${BASE_PATH}/config/config_heuristic_run_ga.json"
+python3 main.py --base_path "$BASE_PATH" --heur_run_config "$CONFIG_PATH_HEUR_RUN" --coordinator_config "$CONFIG_PATH_COORDINATOR"
 
-CONFIG_PATH_HEUR_RUN_GA="${BASE_PATH}/config/config_heuristic_run_ga.json"
-python3 main.py --base_path "$BASE_PATH" --heur_run_config "$CONFIG_PATH_HEUR_RUN_GA" --coordinator_config "$CONFIG_PATH_COORDINATOR"
-CONFIG_PATH_HEUR_RUN_PSO="${BASE_PATH}/config/config_heuristic_run_pso.json"
-python3 main.py --base_path "$BASE_PATH" --heur_run_config "$CONFIG_PATH_HEUR_RUN_PSO" --coordinator_config "$CONFIG_PATH_COORDINATOR"
+# CONFIG_PATH_HEUR_RUN_GA="${BASE_PATH}/config/config_heuristic_run_ga.json"
+# python3 main.py --base_path "$BASE_PATH" --heur_run_config "$CONFIG_PATH_HEUR_RUN_GA" --coordinator_config "$CONFIG_PATH_COORDINATOR"
+# CONFIG_PATH_HEUR_RUN_PSO="${BASE_PATH}/config/config_heuristic_run_pso.json"
+# python3 main.py --base_path "$BASE_PATH" --heur_run_config "$CONFIG_PATH_HEUR_RUN_PSO" --coordinator_config "$CONFIG_PATH_COORDINATOR"
