0b9ed752a872f5d8c0ca227145bc611ef30051cd
Marius Herget <m.herget@uva.nl>
2024-11-22 16:29:29 +0100

    Implemented logger disable options. Default currently to disabled

--- src_main/external/simulation_model/src/utils/logger.py
+++ src_main/external/simulation_model/src/utils/logger.py
@@ -28,19 +28,21 @@ class CustomFormatter(logging.Formatter):
         return s
 
 
-def logger(name, outfolder, print_stdout=False):
+def logger(name, outfolder, print_stdout=False, disabled=True):
     outputfile = os.path.join(outfolder, 'log_'+str(name)+'.txt')
     formatter = CustomFormatter(
         fmt='[ %(name)s  %(asctime)s  %(levelname)-8s ]  %(message)s',
         datefmt='%d-%m-%Y %H:%M:%S'
     )
 
-    handler = logging.FileHandler(outputfile, mode='w')
-    handler.setFormatter(formatter)
-
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
-    logger.addHandler(handler)
+    logger.disabled = disabled
+
+    if not disabled:
+        handler = logging.FileHandler(outputfile, mode='w')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
 
     if (print_stdout):
         screen_handler = logging.StreamHandler(stream=sys.stdout)
