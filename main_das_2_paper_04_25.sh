32427ee73ecf7325576505191762b6d9e037be27
Marius Herget (DAS6) <m.herget@uva.nl>
2025-04-16 20:04:50 +0200

    backup before paper exp 1 - 16-04-25

---
+++ main_das_2_paper_04_25.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Activate upper bash command to set conda environment.
+eval "$(/home/mherget/miniconda3/bin/conda shell.bash hook)"
+
+# TXT from windows to unix_
+# find . -type f -print0 | xargs -0 dos2unix
+
+# Specify the path to the base directory of the hyper-heuristic DSE here.
+BASE_PATH="/var/scratch/mherget/hyper-heuristic-dse-2.0"
+
+# Specify the path to the coordinator configuration file here
+CONFIG_PATH_COORDINATOR="${BASE_PATH}/config/experiment_asml_04_2025/config_coordinator_experiment_asml_04_2025.json"
+
+# Define PYTHONPATH to Herman's framework.
+export PYTHONPATH="${PYTHONPATH}:${BASE_PATH}/src_main/external/simulation_model"
+
+# Define OMNET++ shared library to link loader.
+OMNET_BASE="/var/scratch/mherget/omnet/omnetpp-6.0.1"
+export PATH=$PATH:/var/scratch/mherget/omnet/omnetpp-6.0.1/bin/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${OMNET_BASE}/lib/
+
+cd ${OMNET_BASE}
+source setenv
+cd ../inet4.5
+source setenv
+cd ${BASE_PATH}
+eval "ulimit -n 262000"
+ulimit -n 262000
+
+CONFIG_PATH_EXPERIMENT_1="${BASE_PATH}/config/experiment_asml_04_2025/experiment_asml_04_2025.json"
+
+
+python3 main.py \
+ --experiment asml \
+ --base_path "$BASE_PATH" \
+ --coordinator_config "$CONFIG_PATH_COORDINATOR"
