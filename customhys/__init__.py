6079ad821794336f1dffee57cf123256a8cf2c1a
Marius Herget (DAS6) <m.herget@uva.nl>
2025-06-04 19:12:34 +0200

    cleanup files

--- customhys/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = ['benchmark_func', 'experiment', 'hyperheuristic', 'metaheuristic', 'operators', 'population', 'tools',
-           'visualisation']
-# Under revision: 'characterisation', 'visualisation'
-__version__ = "1.1.6a"
-__author__ = "Jorge M. Cruz-Duarte"
